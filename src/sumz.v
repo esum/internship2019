f71e4ff778c5fd54bcba4376a3f96fbd9bd92737
Benjamin Graillot <benjamin@graillo.tf>
2019-06-14 23:59:52 +0200

    [sumz] Added lemma sumz0

--- src/sumz.v
+++ src/sumz.v
@@ -17,11 +17,22 @@ Proof.
   elim=> [|h r IHr] ; rewrite BigOp.bigopE //.
 Qed.
 
+Lemma sumz0 {T : Type} :
+  forall f (s : seq T), all (eq_op (Znn 0)) [seq f x | x <- s]
+  -> \sumz_(x <- s) f x = Znn 0.
+Proof.
+  move=> f ; elim=> [|h s IHs] ; move=> all_eq_0 ; rewrite BigOp.bigopE //=.
+  rewrite /= in all_eq_0.
+  move/andP in all_eq_0.
+  destruct all_eq_0 as [f_h_eq_0 all_eq_0].
+  move/eqP in f_h_eq_0.
+  rewrite -f_h_eq_0 add0z -BigOp.bigopE.
+  by apply: IHs.
+Qed.
+
 Lemma sumz_div_inv : forall f n,
   \sumz_(d %| n) f d = \sumz_(d %| n) f (n %/ d).
 Admitted.
-
-Search "divisors".
 
 Lemma sumz_div_div : forall f m n,
   \sumz_(d %| m * n) f d = \sumz_(d1 %| m) (\sumz_(d2 %| n) f (d1 * d2)).
